7e61ea9f592a7555df7546e57e727821331e723a
anish <anishdangi.me@gmail.com>
2025-06-26 22:32:30 +0545

    Implementing api key from env

--- src/utility/apiClient.ts
+++ src/utility/apiClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://sky-scrapper.p.rapidapi.com/api/v1",
   headers: {
-    "x-rapidapi-key": "3bd62d74femsh6fea6a3f6edf3e8p1bfceajsn69b468b6967c",
+    "x-rapidapi-key": import.meta.env.VITE_API_ACCESS_KEY,
     "x-rapidapi-host": "sky-scrapper.p.rapidapi.com"
   }
 });
